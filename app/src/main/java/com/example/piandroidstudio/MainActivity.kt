08d059e56f1e8992b166a358c23deba580c21e12
unknown <DAM209@iesataulfo.local>
2025-12-10 20:07:53 +0100

    Se han implementado las funcionalidades de la ventana equipo. Ya se puede navegar entre ventanas

--- app/src/main/java/com/example/piandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/piandroidstudio/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
@@ -62,6 +63,8 @@ class MainActivity : AppCompatActivity() {
         }
         bottomNavEquipos.setOnClickListener {
             Toast.makeText(this, "Equipos clickeado", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, EquipoActivity::class.java)
+            startActivity(intent)
         }
         bottomNavClasificacion.setOnClickListener {
             Toast.makeText(this, "Clasificación clickeado", Toast.LENGTH_SHORT).show()

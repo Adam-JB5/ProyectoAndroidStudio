7fb10ab85a11bd0bb6475d8edea5f94b11b9f954
Adam Janah <ajanah@irontec.com>
2026-04-07 16:26:33 +0200

    Datos simulados para mostrar eventos

--- app/src/main/java/com/example/northfutbol/PartidoActivity.kt
+++ app/src/main/java/com/example/northfutbol/PartidoActivity.kt
@@ -1,8 +1,11 @@
 package com.example.northfutbol
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -31,10 +34,35 @@ class PartidoActivity : AppCompatActivity() {
         Jugador("1", "Sergio Luna", "POR")
     )
 
+    private lateinit var tabAlineacion: TextView
+    private lateinit var tabEventos: TextView
+
+    private lateinit var scrollAlineacion: ScrollView
+    private lateinit var scrollEventos: ScrollView
+
     // Views
     private lateinit var contenedorJugadores: LinearLayout
     private lateinit var btnEquipo1: Button
     private lateinit var btnEquipo2: Button
+
+    data class Evento(
+        val tipoEvento: Char, // 'G', 'M', 'R', 'A'
+        val minuto: Int,
+        val nombre: String
+    )
+
+    // Datos simulados
+    private val eventosPartido = listOf(
+        Evento( 'G', 5, "Manuel Herrera"),    // Gol de jugador 9 (Manuel Herrera)
+        Evento( 'M', 12, "Lucas Romero"),   // Tarjeta amarilla jugador 7 (Lucas Romero)
+        Evento( 'G', 23, "Carlos Peña"),  // Gol de jugador 11 (Carlos Peña)
+        Evento( 'A', 35, "Diego Fernández"),   // Asistencia de jugador 3 (Diego Fernández)
+        Evento( 'R', 44, "Marcos Díaz"),   // Tarjeta roja jugador 4 (Marcos Díaz)
+        Evento('G', 67, "Lucas Romero"),   // Gol jugador 7 (Lucas Romero)
+        Evento( 'G', 89, "Iván Soto")    // Gol jugador 8 (Iván Soto)
+    )
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,38 +74,89 @@ class PartidoActivity : AppCompatActivity() {
 
         // Referencias
         contenedorJugadores = findViewById(R.id.listaJugadores)
+
+        // Tabs
+        tabAlineacion = findViewById(R.id.tabAlineacion)
+        tabEventos = findViewById(R.id.tabEventos)
+
+        // Botones equipo alineacion
         btnEquipo1 = findViewById(R.id.btnEquipo1)
         btnEquipo2 = findViewById(R.id.btnEquipo2)
 
+        //Secciones
+        scrollAlineacion = findViewById(R.id.scrollAlineacion)
+        scrollEventos = findViewById(R.id.scrollEventos)
+
         // Equipo inicial
-        cargarEquipo(equipo1)
+        cargarJugadoresEquipo(equipo1)
         marcarEquipoActivo(btnEquipo1, btnEquipo2)
 
         // Listeners
         btnEquipo1.setOnClickListener {
-            cargarEquipo(equipo1)
+            cargarJugadoresEquipo(equipo1)
             marcarEquipoActivo(btnEquipo1, btnEquipo2)
         }
 
         btnEquipo2.setOnClickListener {
-            cargarEquipo(equipo2)
+            cargarJugadoresEquipo(equipo2)
             marcarEquipoActivo(btnEquipo2, btnEquipo1)
+        }
+
+        tabAlineacion.setOnClickListener {
+            mostrarAlineacion()
+        }
+
+        tabEventos.setOnClickListener {
+            mostrarEventos()
+            cargarEventosPartido()
         }
     }
 
-    private fun cargarEquipo(jugadores: List<Jugador>) {
-        for (i in 0 until contenedorJugadores.childCount) {
-            val item = contenedorJugadores.getChildAt(i)
+    private fun cargarJugadoresEquipo(jugadores: List<Jugador>) {
+        contenedorJugadores.removeAllViews()
+
+        for (jugador in jugadores) {
+            val item = layoutInflater.inflate(R.layout.item_jugador, contenedorJugadores, false)
 
             val txtNumero = item.findViewById<TextView>(R.id.txtNumero)
             val txtNombre = item.findViewById<TextView>(R.id.txtNombre)
             val txtPosicion = item.findViewById<TextView>(R.id.posicion)
 
-            val jugador = jugadores[i]
-
             txtNumero.text = jugador.numero
             txtNombre.text = jugador.nombre
             txtPosicion.text = jugador.posicion
+
+            contenedorJugadores.addView(item)
+        }
+    }
+
+    private fun cargarEventosPartido() {
+        val contenedorEventos = findViewById<LinearLayout>(R.id.listaEventos)
+        contenedorEventos.removeAllViews()
+
+        for (evento in eventosPartido) {
+            val item = layoutInflater.inflate(R.layout.item_evento, contenedorEventos, false)
+
+            val txtMinuto = item.findViewById<TextView>(R.id.tvMinuto)
+            val imgTipoEvento = item.findViewById<ImageView>(R.id.ivTipoEvento)
+            val txtNombreJugador = item.findViewById<TextView>(R.id.tvNombreJugador)
+
+            // Mostrar minuto
+            txtMinuto.text = "${evento.minuto}'"
+
+            // Mostrar nombre del jugador
+            txtNombreJugador.text = evento.nombre
+
+            // Asignar icono según tipo de evento
+            when (evento.tipoEvento) {
+                'G' -> imgTipoEvento.setImageResource(R.drawable.goal)   // Gol
+                'M' -> imgTipoEvento.setImageResource(R.drawable.yellow_card) // Amarilla
+                'R' -> imgTipoEvento.setImageResource(R.drawable.red_card) // Roja
+                'A' -> imgTipoEvento.setImageResource(R.drawable.assist)  // Asistencia
+                else -> imgTipoEvento.setImageResource(R.drawable.home)
+            }
+
+            contenedorEventos.addView(item)
         }
     }
 
@@ -85,5 +164,27 @@ class PartidoActivity : AppCompatActivity() {
         activo.backgroundTintList = null
         inactivo.backgroundTintList =
             ContextCompat.getColorStateList(this, R.color.gris_texto)
+    }
+
+    private fun mostrarAlineacion() {
+        scrollAlineacion.visibility = View.VISIBLE
+        scrollEventos.visibility = View.GONE
+
+        tabAlineacion.setBackgroundResource(R.drawable.bg_tab_selected)
+        tabEventos.setBackgroundResource(android.R.color.transparent)
+
+        tabAlineacion.setTextColor(ContextCompat.getColor(this, R.color.negro))
+        tabEventos.setTextColor(ContextCompat.getColor(this, R.color.gris_texto))
+    }
+
+    private fun mostrarEventos() {
+        scrollAlineacion.visibility = View.GONE
+        scrollEventos.visibility = View.VISIBLE
+
+        tabEventos.setBackgroundResource(R.drawable.bg_tab_selected)
+        tabAlineacion.setBackgroundResource(android.R.color.transparent)
+
+        tabEventos.setTextColor(ContextCompat.getColor(this, R.color.negro))
+        tabAlineacion.setTextColor(ContextCompat.getColor(this, R.color.gris_texto))
     }
 }
